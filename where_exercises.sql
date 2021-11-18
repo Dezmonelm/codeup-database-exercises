d05b3ebf6862cc1cf5caea3985690477a220c9a5
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-11-18 16:27:09 -0500

    finishes where

---
+++ where_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') AND gender = 'M';
+
+SELECT * FROM employees WHERE last_name Like 'E%';
+
+SELECT * FROM employees WHERE last_name Like '%q%';
+
+SELECT * FROM employees WHERE last_name like '%E%' AND  last_name LIKE '%E%';
+
+SELECT * FROM employees WHERE last_name like '%q%' AND  last_name  NOT LIKE '%qu%';
